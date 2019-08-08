964113c57e6b79c303eadc915b4468a496090694
hijiii <tumujidenka@gmail.com>
2019-08-08 20:41:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(function(){
-  $('.pageLink').click(function(){
+
+  $('.contentsList').click(function(){
+    var $detail = $(this).find('contentsList_Detail');
+    if($detail).hasClass('open'){
+      $detail.removeClass('open');
+      $detail.slideUp();
+    }else {
+    $detail.addClass('open');
+    $detail.slideDown();
+    };
+  });
+
+  $(".pageLink").click(function(){
     var linkTo = $(this).attr('href');
     var linkPosition = $(linkTo).offset().top;
     var scrollSpeed = 500;
@@ -7,5 +19,5 @@ $(function(){
     $('html,body').animate({
       'scrollTop':linkPosition
     },scrollSpeed);
-  };
+  });
 });
